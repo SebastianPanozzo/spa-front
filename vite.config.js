fc6786b42cda424196c2ecf3df1e94275dc692ba
Sebastian Panozzo <panozzosebastian9@gmail.com>
2025-04-25 17:57:12 -0300

    Primer commitsss

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Asegúrate de que sea 'dist'
+    outDir: 'dist', // Asegúrate de que la salida se haga en la carpeta 'dist'
+  },
+  resolve: {
+    alias: {
+      '@': '/src'  // Esto configura '@' para que apunte a la carpeta 'src'
+    }
   }
 })
